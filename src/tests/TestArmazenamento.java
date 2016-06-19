426be553ddc1dda8614e0fe401cd214468fea980
Flávio Cardoso Ferreira Pontes <flaviocpontes@gmail.com>
2016-06-19 19:22:35 -0300

    Armazenamento em dois usuários falhando.

--- src/tests/TestArmazenamento.java
+++ src/tests/TestArmazenamento.java
@@ -8,10 +8,19 @@ import org.junit.Test;
 public class TestArmazenamento {
 
     @Test
-    public void testStoringPoints(){
+    public void testeArmazenandoUmTipoEmUmUsuario(){
         Armazenamento arm = new Armazenamento();
         arm.gravaPontuacao("guerra", "estrela", 10);
         assertEquals(arm.lePontuacao("guerra", "estrela"), 10);
+    }
+
+    @Test
+    public void testeArmazenandoUmTipoEmDoisUsuarios(){
+        Armazenamento arm = new Armazenamento();
+        arm.gravaPontuacao("guerra", "estrela", 10);
+        arm.gravaPontuacao("fernandes", "estrela", 14);
+        assertEquals(arm.lePontuacao("guerra", "estrela"), 10);
+        assertEquals(arm.lePontuacao("fernandes", "estrela"), 14);
     }
 
 }
